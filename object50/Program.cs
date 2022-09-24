5ba7f05b4fed192a3ba9840ea5adda8954166a52
Nikolay Lebedinskiy <nikolaylebedinskiy1983.ru>
2022-09-24 19:20:15 +0300

    Задача 50

---
+++ object50/Program.cs
@@ -0,0 +1,59 @@
+﻿int [,] GenerateArray(int rows, int columns, int minrnd, int maxrnd)
+{
+    int [,] array = new int [rows, columns];
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+          array[i, j] = rnd.Next(minrnd, maxrnd + 1);  
+        }
+    }
+    return array;
+}
+
+void PrintArray (int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            System.Console.Write($"{matrix[i, j]}\t");
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    int result = Convert.ToInt32(Console.ReadLine());
+    return result;
+}
+
+(int, bool) IndexValue(int[,] array, int row, int column) 
+{
+  if (row < 0 && row > array.GetLength(0) && column < 0 && column > array.GetLength(1))
+  {
+    return (0, false);
+  }
+  else
+  {
+    return (array[row, column], true);
+  }
+}
+
+int[,] NewArray = GenerateArray(5, 5, -100, 100);
+PrintArray(NewArray);
+int row = Prompt("Введите номер строки массива: ");
+int column = Prompt("Введите номер столбца массива: ");
+(int result, bool isEx) = IndexValue(NewArray, row, column);
+if (isEx)
+{
+    System.Console.WriteLine($"Элемент массива равен: {result}");
+}
+else
+{
+    System.Console.WriteLine("Такого элемента в массиве нет");
+}
